467c6c2e09f3a9090e07d1be0e609c58c2c2e763
Reshait <reshait@outlook.com>
2016-03-03 14:14:45 +0100

    Añadido por Carlos
    Hecho por Carlos en clase.

--- practica2/main.cpp
+++ practica2/main.cpp
@@ -5,4 +5,59 @@ class NeighOperatorKP{
     SolutionKP* generateNeight(SolutionKP* sCurrent, int pos){
         //ESTO ES DEMASIADO FÁCIL
     }
+};
+
+class FirstNeighExplorator : public AbstractNegh...or{
+protected:
+    Random *random;
+    AbstractProblemInstance *problemInstance;
+public:
+    FirstNeighExplorator(AbstractProblemInstance *problemInstance, Random *random){
+        this.random = random;
+        //algo como this.problemInstance...
+    }
+    SolutionKP* exploreNg(SolutionKP* sCurrent, AbstractNeighOperatorKP *operator){
+
+        Solution *neighSol = NULL;
+
+        int numPosibilities = operator.getNumPosParam();
+
+        for(int i = 0; i < numPosibilities; i++){
+            neightSol = operator.generateNeigh(sCurrent, 0);
+            problemInstance.evaluate(neightSol);
+
+            if(problemInstance.isBetter(neighSol, sCurrent)){
+                return neightSol;
+            }
+        }
+
+        return neighSol;
+    }
+};
+
+class LocalSearch{
+protected:
+    AbstractNeighOperator *operator;
+    Random *random;
+    AbstractNeightExplorator *explorator;
+
+public:
+    LocalSearch(....){
+        this.random = random;
+        //--> cree que lo podría eliminar -->this.operator = operator;
+
+    }
+
+    Solution* applyLS(Solution* initialSol){
+        Solution* currentSol = initialSol ->copy();
+        Solution* neigh = explorator.exploreNg(currentSol);
+
+        while(neigh != NULL){
+            currentSol ->copy(neigh);
+            neigh = explorator.exploreNg(currentSol);
+        }
+
+        delete neigh;
+        return currentSol;
+    }
 }
