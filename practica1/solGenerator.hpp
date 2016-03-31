e805f1e5c6210323f552cc52ebdce3bfc687e07c
Reshait <reshait@outlook.com>
2016-03-31 14:20:29 +0200

    NO -> srand

--- practica1/solGenerator.hpp
+++ practica1/solGenerator.hpp
@@ -23,20 +23,38 @@ class SolGeneratorKP{
 		inline void generaSolucion(InstanceKP I){
 			SolutionKP S;
 			int i;
+			vector<Objeto>::iterator j;
 
 			srand(time(NULL));
 			
 			while(S.getPesoTotal() <  I.getPesoMax()){
 				i = rand() % 200;
+				cout << "Aleatorio escogido ..: " << i << endl;
 
-				if(!I.vector_[i].getEnMochila()){
+/*				if(!I.vector_[i].getEnMochila()){
 					S.vSol_[i] = 1;
 					I.vector_[i].setEnMochila(1);
 					S.setBeneficioTotal(S.getBeneficioTotal() + I.vector_[i].getBeneficio());
 					S.setPesoTotal(S.getPesoTotal() + I.vector_[i].getPeso());
 				}
-				
+*/
+				for(j = I.vector_.begin(); j < I.vector_.end(); j++){
+					if(i == j->getPosicion()){
+						if(!j->getEnMochila()){
+							j->setEnMochila(1);
+							S.setBeneficioTotal(S.getBeneficioTotal() + j->getBeneficio());
+							S.setPesoTotal(S.getPesoTotal() + j->getPeso());
+							//S.vSol_[i] = 1;  --> Me falta por conseguir esto
+							break;
+						}
+
+						break;
+					}
+				}				
 			}
+
+			cout << "Beneficio obtenido\t ..: " << S.getBeneficioTotal() << endl;
+			cout << "Peso Total obtenido\t ..: " << S.getPesoTotal() << endl;
 			aniadeElementoLista(S);
 		}
 };
