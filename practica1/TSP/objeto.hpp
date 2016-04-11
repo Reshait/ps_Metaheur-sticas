e9aa04e71ef83ce8dc9f0fea691c43138fabc0e9
Reshait <reshait@outlook.com>
2016-04-11 19:18:10 +0200

    Empenzando con TSP

--- practica1/TSP/objeto.hpp
+++ practica1/TSP/objeto.hpp
@@ -6,19 +6,16 @@ class Objeto{
 		float peso_;
 		float beneficio_;
 		int posicion_;
-//		bool enMochila_;
 
 	public:
 		Objeto(){}
 		inline float getPeso(){ return peso_; }
 		inline float getBeneficio(){ return beneficio_; }
 		inline int getPosicion(){ return posicion_; }
-		inline bool getEnMochila(){ return enMochila_; }
 
 		inline void setPeso(float peso){ peso_ = peso; }
 		inline void setBeneficio(float beneficio){ beneficio_ = beneficio; }
 		inline void setPosicion(int posicion){ posicion_ = posicion; }
-		inline void setEnMochila(bool enMochila){ enMochila_ = enMochila; }
 };
 
 #endif
